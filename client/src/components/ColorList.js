b02c3e5713470bebe3a1ab69ed74d49a2fff6287
Ryan Erickson <rerickson21@yahoo.com>
2020-07-19 21:44:42 -0400

    Changed password

--- client/src/components/ColorList.js
+++ client/src/components/ColorList.js
@@ -1,31 +1,55 @@
 import React, { useState } from "react";
 import axios from "axios";
-
+import { axiosWithAuth } from "../utilities/axiosWithAuth";
 const initialColor = {
   color: "",
   code: { hex: "" }
 };
-
 const ColorList = ({ colors, updateColors }) => {
   console.log(colors);
   const [editing, setEditing] = useState(false);
   const [colorToEdit, setColorToEdit] = useState(initialColor);
+  const [newColor, setNewColor] = useState(initialColor);
 
   const editColor = color => {
     setEditing(true);
     setColorToEdit(color);
   };
-
   const saveEdit = e => {
     e.preventDefault();
     // Make a put request to save your updated color
     // think about where will you get the id from...
     // where is is saved right now?
+    axiosWithAuth().put(`http://localhost:5000/api/colors/${colorToEdit.id}`, colorToEdit)
+    .then(response => {
+      console.log(response)
+      axiosWithAuth().get('http://localhost:5000/api/colors')
+      .then(response => {
+        updateColors(response.data)
+      })
+      .catch(error => console.log(error))
+    })
+    .catch(error => console.log(error))
   };
-
   const deleteColor = color => {
     // make a delete request to delete this color
+    axiosWithAuth().delete(`http://localhost:5000/api/colors/${color.id}`)
+    .then(response => {
+      console.log(response)
+      updateColors(colors.filter(colorId => colorId.id !== color.id ))
+    })
+    .catch(error => console.log(error))
   };
+
+  const submitNewColor = event => {
+    event.preventDefault();
+    axiosWithAuth().post('http://localhost:5000/api/colors', newColor)
+    .then(response => {
+      updateColors(response.data)
+      setNewColor(initialColor);
+    })
+    .catch(error => console.log(error))
+  }
 
   return (
     <div className="colors-wrap">
@@ -80,10 +104,26 @@ const ColorList = ({ colors, updateColors }) => {
           </div>
         </form>
       )}
+
+      <form onSubmit={submitNewColor}>
+        <label htmlFor='color'>Color: </label>
+        <input type='text' name='color' id='color' value={newColor.color} onChange={(event) => {
+          setNewColor({
+            ...newColor,
+            color: event.target.value
+          })
+        }} />
+        <label htmlFor='hex'>Hex No. </label>
+        <input type='text' name='hex' id='hex' value={newColor.code.hex} onChange={(event) => {
+          setNewColor({
+            ...newColor,
+            code: { hex: event.target.value}
+          })
+        }} />
+        <button type='submit'>Add your own color!</button>
+      </form>
       <div className="spacer" />
-      {/* stretch - build another form here to add a color */}
-    </div>
+     </div>
   );
 };
-
 export default ColorList;
