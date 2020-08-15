3ceb706b87d69fa726871fbaa16c7220350990ee
JenVest2020 <jenvest2019@gmail.com>
2020-08-14 21:26:44 -0500

    axiosWithAuth&PrivateRoute built

---
+++ client/src/utils/PrivateRoute.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import { Route, Redirect } from 'react-router-dom';
+
+const PrivateRoute = ({ component: Component, ...restOfProps }) => {
+    return (
+        <Route
+            {...restOfProps}
+            render={(props =>
+                localStorage.getItem('token') ? (
+                    <Component {...props} />
+                ) : (
+                        <Redirect to='/login' />
+                    )
+            )}
+        />
+    )
+}
