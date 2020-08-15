e098ad34e09c3d9e78ae46f03a1e36ee465ebd01
JenVest2020 <jenvest2019@gmail.com>
2020-08-14 22:04:28 -0500

    Login Completed

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,28 +1,53 @@
-import React from "react";
+import React, { useState } from "react";
+import { axiosWithAuth } from '../utils/axiosWithAuth';
+import { useHistory } from 'react-router-dom';
 
 const Login = () => {
-  // make a post request to retrieve a token from the api
-  // when you have handled the token, navigate to the BubblePage route
+  const [login, setLogin] = useState({
+    username: '',
+    password: ''
+  })
+
+  const handleChanges = (e) => {
+    setLogin({
+      ...login,
+      [e.target.name]: e.target.value
+    })
+    console.log('from Login: hC: login', login)
+  }
+
+  const history = useHistory();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    axiosWithAuth()
+      .post('http://localhost:5000/api/login', login)
+      .then(res => {
+        console.log('from Login: post successful: res', res);
+        localStorage.setItem('token', res.data.payload)
+      })
+    history.push('/bubblepage')
+  }
   return (
     <>
       <h1>Welcome to the Bubble App!</h1>
-      <form>
+      <form onSubmit={handleSubmit}>
         <label htmlFor='username'>User Name</label>
-        <input 
-        type='text' 
-        name='username'
-        placeholder='User Name'
-        // value={}
-        // onChange={} 
-        className='input' />
+        <input
+          type='text'
+          name='username'
+          placeholder='User Name'
+          value={login.username}
+          onChange={handleChanges}
+          className='input' />
         <label htmlFor='password'>Password</label>
-        <input 
-        type='text' 
-        name='password'
-        placeholder='Password'
-        // value={}
-        // onChange={} 
-        className='input' />
+        <input
+          type='text'
+          name='password'
+          placeholder='Password'
+          value={login.password}
+          onChange={handleChanges}
+          className='input' />
         <button>Login</button>
       </form>
     </>
--- client/src/utils/axiosWithAuth.js
+++ client/src/utils/axiosWithAuth.js
@@ -4,7 +4,7 @@ export const axiosWithAuth = () => {
     const token = localStorage.getItem('token');
 
     return axios.create({
-        baseURL: 'http://localhost:3000/api/',
+        baseURL: 'http://localhost:5000/api/',
         headers: {
             Authorization: token
         }
