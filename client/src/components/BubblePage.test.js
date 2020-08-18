e677af853014938d271be3dae0054d7587f4afd7
Dustin Myers <dustin@lambdaschool.com>
2020-08-18 12:36:37 -0600

    Add test file

---
+++ client/src/components/BubblePage.test.js
@@ -0,0 +1,7 @@
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import BubblePage from "./BubblePage";
+
+test("Fetches data and renders the bubbles", () => {
+  // Finish this test
+});
