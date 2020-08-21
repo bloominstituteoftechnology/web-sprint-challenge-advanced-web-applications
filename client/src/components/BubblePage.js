00a623e1b6a235903bb68648b5eb9c94560aeac2
MMendes24 <mmbusiness1@protonmail.com>
2020-08-21 10:42:31 -0400

    Bubble page is fetching colors.

--- client/src/components/BubblePage.js
+++ client/src/components/BubblePage.js
@@ -3,11 +3,26 @@ import axios from "axios";
 
 import Bubbles from "./Bubbles";
 import ColorList from "./ColorList";
+import axiosWithAuth from "../utils/axiosWithAuth";
 
 const BubblePage = () => {
   const [colorList, setColorList] = useState([]);
   // fetch your colors data from the server when the component mounts
   // set that data to the colorList state property
+  const getColors = () => {
+    axiosWithAuth()
+    .get("/api/colors")
+    .then(res => {
+      setColorList(res.data)
+    })
+    .catch(err => {
+      console.error("You absolute, ABSOLUTE buffoon!")
+    })
+  }
+
+  useEffect(() => {
+    getColors()
+  }, [])
 
   return (
     <>
