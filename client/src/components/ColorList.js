384eda61ff97f1dee099acfd4fa61bbd0d32fa24
MMendes24 <mmbusiness1@protonmail.com>
2020-08-21 11:06:25 -0400

    You can now edit colors! It's beautiful.

--- client/src/components/ColorList.js
+++ client/src/components/ColorList.js
@@ -1,5 +1,7 @@
 import React, { useState } from "react";
+import { useParams } from "react-router-dom";
 import axios from "axios";
+import axiosWithAuth from "../utils/axiosWithAuth";
 
 const initialColor = {
   color: "",
@@ -10,6 +12,7 @@ const ColorList = ({ colors, updateColors }) => {
   console.log(colors);
   const [editing, setEditing] = useState(false);
   const [colorToEdit, setColorToEdit] = useState(initialColor);
+  const { id } = useParams();
 
   const editColor = color => {
     setEditing(true);
@@ -21,6 +24,20 @@ const ColorList = ({ colors, updateColors }) => {
     // Make a put request to save your updated color
     // think about where will you get the id from...
     // where is is saved right now?
+    axiosWithAuth()
+    .put(`/api/colors/${id}`, colorToEdit)
+    .then( res => {
+      updateColors([
+        ...colors.map(color => {
+          if ( color.id === colorToEdit.id ) {
+            return colorToEdit
+          } else {
+            return color
+          }
+        })
+      ])
+    })
+   
   };
 
   const deleteColor = color => {
