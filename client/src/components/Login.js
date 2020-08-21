18857b726c069913252048aa2ad9ba9a6515ddb5
chaycesol <chayce.solchaga@gmail.com>
2020-08-21 09:28:26 -0700

    created PR component and starting login

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,5 +1,7 @@
-import React from "react";
+import React, { useState } from "react";
+import { useHistory } from 'react-router-dom';
 
+import { axiosWithAuth } from '../utils/axiosWithAuth';
 const Login = () => {
   // make a post request to retrieve a token from the api
   // when you have handled the token, navigate to the BubblePage route
