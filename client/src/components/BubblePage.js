3057f90310059a085f9716872ab24302d8975b87
unknown <nicholasnobida@yahoo.com>
2020-08-21 16:48:22 -0700

    current progress

--- client/src/components/BubblePage.js
+++ client/src/components/BubblePage.js
@@ -1,13 +1,21 @@
 import React, { useState, useEffect } from "react";
-import axios from "axios";
 
 import Bubbles from "./Bubbles";
 import ColorList from "./ColorList";
+import fetchApi from './fetchApi';
 
 const BubblePage = () => {
   const [colorList, setColorList] = useState([]);
   // fetch your colors data from the server when the component mounts
   // set that data to the colorList state property
+
+  useEffect(() => {
+    fetchApi()
+      .then((res) => {
+        setColorList(res.data)
+        console.log(res.data)
+      });
+  }, []);
 
   return (
     <>
--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,12 +1,55 @@
-import React from "react";
+import React, { useState } from "react";
+import axiosWithAuth from './axiosWithAuth';
 
-const Login = () => {
+const Login = (props) => {
   // make a post request to retrieve a token from the api
   // when you have handled the token, navigate to the BubblePage route
+
+  const [ credentials, setCredentials ] = useState({
+    username:'',
+    password:''
+  })
+
+  const handleChange = (e) => {
+    setCredentials({
+      ...credentials,
+      [e.target.name]: e.target.value
+    })
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    axiosWithAuth()
+      .post('/api/login', credentials)
+      .then((res) => {
+        localStorage.setItem('token', res.data.payload)
+        props.history.push('/bubble')
+      })
+      .catch(err => console.log(err))
+  }
+
   return (
     <>
       <h1>Welcome to the Bubble App!</h1>
-      <p>Build a login page here</p>
+      <form onSubmit={handleSubmit}>
+        <input
+          type='text'
+          name='username'
+          value={credentials.username}
+          onChange={handleChange}
+          placeholder='username'
+        />
+        <input
+          type='text'
+          name='password'
+          value={credentials.password}
+          onChange={handleChange}
+          placeholder='password'
+        />
+        <button>
+          Submit
+        </button>
+      </form>
     </>
   );
 };
