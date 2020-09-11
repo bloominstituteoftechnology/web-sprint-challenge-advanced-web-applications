e024bd880eb015610ac742d21a4ffa91459be04d
Elizabeth Hagag <lizjanegiles@gmail.com>
2020-09-11 18:32:36 -0500

    Bubble Page Axios working'

--- client/src/components/BubblePage.js
+++ client/src/components/BubblePage.js
@@ -1,13 +1,32 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
-
+import {axiosWithAuth} from '../utils/axiosWithAuth'
 import Bubbles from "./Bubbles";
 import ColorList from "./ColorList";
 
 const BubblePage = () => {
-  const [colorList, setColorList] = useState([]);
+  const [colorList, setColorList] = useState([{
+    color: "",
+    code: {
+    hex: ""
+  },
+    id: ""}] );
+
   // fetch your colors data from the server when the component mounts
   // set that data to the colorList state property
+
+  useEffect(() => {
+    axiosWithAuth()
+    .get("api/colors")
+    .then((res) => {
+      setColorList(res.data)
+      console.log("Color API Working", res)
+    })
+    .catch((err) => {
+      console.log("BubblePage Get Axios Not Working", err)
+    })
+  })
+
 
   return (
     <>
