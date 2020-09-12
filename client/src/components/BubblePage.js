ddb707e6696aebe0a477e570bf82a15d276d5fd2
Terry Edwards <terryedwardsjr113@gmail.com>
2020-09-12 14:50:44 -0400

    working

--- client/src/components/BubblePage.js
+++ client/src/components/BubblePage.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
-import axios from "axios";
-
+// import axios from "axios";
+import {axiosWithAuth} from './axiosWithAuth';
 import Bubbles from "./Bubbles";
 import ColorList from "./ColorList";
 
@@ -9,11 +9,25 @@ const BubblePage = () => {
   // fetch your colors data from the server when the component mounts
   // set that data to the colorList state property
 
+  const getColors=(()=>{
+    axiosWithAuth()
+    .get('/colors')
+    .then((res)=>{
+      console.log(res)
+      // setColorList({colorList:res})
+    })
+    .catch(err=>console.log(err))
+  })
+
   return (
     <>
       <ColorList colors={colorList} updateColors={setColorList} />
       <Bubbles colors={colorList} />
+         <div>
+      <button onClick={getColors}>get colors</button>
+    </div> 
     </>
+
   );
 };
 
--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,14 +1,53 @@
-import React from "react";
+import React,{useState} from "react";
+import axios from 'axios';
 
-const Login = () => {
+
+class Login extends React.Component{
   // make a post request to retrieve a token from the api
   // when you have handled the token, navigate to the BubblePage route
-  return (
+  // const defaultState=
+  //    {username:'',
+  //     password:''}
+  
+  state={
+    credentials: {
+        username: "",
+        password: ""
+    }
+}
+  handleChange=(e)=>{
+    this.setState({
+        credentials:{
+            ...this.state.credentials,
+            [e.target.name]:e.target.value
+        }
+    })
+}
+
+login=(e)=>{
+    e.preventDefault();
+    axios
+    .post('http://localhost:5000/api/login',this.state.credentials)
+    .then((res)=>{
+        localStorage.setItem('token',res.data.payload)
+        this.props.history.push('/protected/BubblePage');
+    }
+    ) 
+    .catch((error=>console.log(error)));
+}
+  render(){
+    return (
     <>
       <h1>Welcome to the Bubble App!</h1>
-      <p>Build a login page here</p>
+      <div>
+        <form onSubmit={this.login}>
+          <input name='username' type='text' placeholder='username' value={this.state.credentials.username} onChange={this.handleChange}/>
+          <input name='password' type='text' placeholder='password' value={this.state.credentials.password} onChange={this.handleChange}/>
+          <button>Login</button>
+        </form>
+      </div>
     </>
-  );
+  );}
 };
 
 export default Login;
