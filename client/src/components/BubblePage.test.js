a6e71d726abbc32d0ac0639f36453f217fecf776
Mason Davis <masondavis2009@gmail.com>
2020-09-13 19:19:07 -0500

    Mvp Complete

--- client/src/components/BubblePage.test.js
+++ client/src/components/BubblePage.test.js
@@ -1,7 +1,97 @@
 import React from "react";
-import { render, screen } from "@testing-library/react";
-import BubblePage from "./BubblePage";
+import { queryAllByTestId, render } from "@testing-library/react";
+import ColorList from "./ColorList";
 
-test("Fetches data and renders the bubbles", () => {
-  // Finish this test
+let colorsData = [
+  {
+    color: "aliceblue",
+    code: {
+      hex: "#f0f8ff"
+    },
+    id: 1
+  },
+  {
+    color: "limegreen",
+    code: {
+      hex: "#99ddbc"
+    },
+    id: 2
+  },
+  {
+    color: "aqua",
+    code: {
+      hex: "#00ffff"
+    },
+    id: 3
+  },
+  {
+    color: "aquamarine",
+    code: {
+      hex: "#7fffd4"
+    },
+    id: 4
+  },
+  {
+    color: "lilac",
+    code: {
+      hex: "#9a99dd"
+    },
+    id: 5
+  },
+  {
+    color: "softpink",
+    code: {
+      hex: "#dd99ba"
+    },
+    id: 6
+  },
+  {
+    color: "bisque",
+    code: {
+      hex: "#dd9a99"
+    },
+    id: 7
+  },
+  {
+    color: "softyellow",
+    code: {
+      hex: "#dcdd99"
+    },
+    id: 8
+  },
+  {
+    color: "blanchedalmond",
+    code: {
+      hex: "#ffebcd"
+    },
+    id: 9
+  },
+  {
+    color: "blue",
+    code: {
+      hex: "#6093ca"
+    },
+    id: 10
+  },
+  {
+    color: "blueviolet",
+    code: {
+      hex: "#8a2be2"
+    },
+    id: 11
+  }
+];
+
+
+test("Fetches data", () => {
+
+  const { rerender, queryAllByTestId } = render(<ColorList colors={[]} />)
+
+  expect(queryAllByTestId(/colors/i)).toHaveLength(0);
+
+  rerender(<ColorList colors={colorsData} />)
+  const colors = queryAllByTestId(/colors/i)
+  expect(colors).toHaveLength(11)
 });
+
+
