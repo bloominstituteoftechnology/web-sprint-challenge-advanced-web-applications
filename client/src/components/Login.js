88606e5c66996c1f526a1ab4daca47cd8eca854b
Luke Adams <muhammadadams@gmail.com>
2020-09-13 21:33:26 -0400

    login added

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,13 +1,50 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
 
 const Login = () => {
   // make a post request to retrieve a token from the api
   // when you have handled the token, navigate to the BubblePage route
+  // Need state
+  // HandleChange and HandleSubmit
+
+  const [data, setData] = usestate({
+    userName: "",
+    password: "",
+  });
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+
+  handleChange = (e) => {
+    setData({
+      ...data,
+      [e.target.name]: e.target.value,
+    });
+  };
+
   return (
-    <>
-      <h1>Welcome to the Bubble App!</h1>
-      <p>Build a login page here</p>
-    </>
+    <div className="Login">
+      <form onSubmit={handleSubmit}>
+        <input
+          type="name"
+          name="username"
+          id="username"
+          value={userName}
+          onChange={handleChange}
+        />
+
+        <input
+          type="password"
+          name="password"
+          id="password"
+          value={password}
+          onChange={handleChange}
+        />
+
+        <Button type="submit">Login</Button>
+      </form>
+    </div>
   );
 };
 
