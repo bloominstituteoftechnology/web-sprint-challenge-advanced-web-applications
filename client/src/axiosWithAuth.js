91dcc5b81a957e2de1f23b539bda1d5cd5a9371f
ajg7 <ajgebara@gmail.com>
2020-09-18 10:43:33 -0500

    stored token in local storage and received json back from the server

--- client/src/axiosWithAuth.js
+++ client/src/axiosWithAuth.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const axiosWithAuth = () => {
-    const token = useLocalStorage.getItem("token");
+    const token = localStorage.getItem("token");
 
     return axios.create({
         headers: {
--- client/src/components/BubblePage.js
+++ client/src/components/BubblePage.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
+import { axiosWithAuth } from "../axiosWithAuth";
 
 import Bubbles from "./Bubbles";
 import ColorList from "./ColorList";
@@ -8,6 +9,17 @@ const BubblePage = () => {
   const [colorList, setColorList] = useState([]);
   // fetch your colors data from the server when the component mounts
   // set that data to the colorList state property
+
+  useEffect(() => {
+    axiosWithAuth().get("/api/colors")
+      .then(response => {
+        console.log(response)
+        localStorage.setItem("token", response.config.headers.Authorization)
+      })
+      .catch(error => {
+        console.log(error);
+      })
+  })
 
   return (
     <>
