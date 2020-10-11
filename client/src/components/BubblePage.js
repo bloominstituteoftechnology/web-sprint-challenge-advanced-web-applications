b0dff41ed4697b5652f7b327f6a0ebe4350982ca
Daniel Terry <daniel.terry2121@gmail.com>
2020-10-11 12:49:17 -0400

    Finished Crud Operations starting Testing

--- client/src/components/BubblePage.js
+++ client/src/components/BubblePage.js
@@ -1,14 +1,31 @@
 import React, { useState, useEffect } from "react";
-import axios from "axios";
-
+import { axiosWithAuth } from '../api/axiosWithAuth';
+import { useParams } from "react-router-dom";
 import Bubbles from "./Bubbles";
 import ColorList from "./ColorList";
 
 const BubblePage = () => {
   const [colorList, setColorList] = useState([]);
+  const params = useParams();
+  //getting color id set up with params
   // fetch your colors data from the server when the component mounts
   // set that data to the colorList state property
-  
+  const getData = () => {
+    axiosWithAuth()
+    .get('/colors')
+    .then((res) => {
+        console.log(res)
+        setColorList(res.data)
+    })
+    .catch((err) => {
+        console.log(err)
+    
+    })
+}
+useEffect(() => {
+    getData();
+    console.log(colorList);
+}, []);
 
   return (
     <>
--- client/src/components/ColorList.js
+++ client/src/components/ColorList.js
@@ -1,5 +1,5 @@
-import React, { useState } from "react";
-import axios from "axios";
+import React, { useEffect, useState } from "react";
+import { axiosWithAuth } from "../api/axiosWithAuth";
 
 const initialColor = {
   color: "",
@@ -11,20 +11,48 @@ const ColorList = ({ colors, updateColors }) => {
   const [editing, setEditing] = useState(false);
   const [colorToEdit, setColorToEdit] = useState(initialColor);
 
+  //setting up the edit color function onclick takes. no clue why that isn't throwing an error
   const editColor = color => {
     setEditing(true);
     setColorToEdit(color);
   };
-
+  //setting up a function to re-call color data after making changes 
+  const reColor = () => {
+    axiosWithAuth().get('/colors')
+          .then(res => {
+            updateColors(res.data);
+          })
+          .catch(err => {
+            console.error(err);
+          })
+  }
   const saveEdit = e => {
     e.preventDefault();
     // Make a put request to save your updated color
     // think about where will you get the id from...
     // where is is saved right now?
-  };
+    axiosWithAuth()
+    .put(`/colors/${colorToEdit.id}`, colorToEdit )
+    .then((res) => {
+        console.log(res)
+        reColor()
+      })
+      .catch(err => {
+        console.error(err)
+      })
+}
 
   const deleteColor = color => {
     // make a delete request to delete this color
+    axiosWithAuth()
+          .delete(`/colors/${color.id}`)
+          .then((res) => {
+            console.log(res);
+            reColor()
+        })
+        .catch(err => {
+          console.error(err);
+        })
   };
 
   return (
