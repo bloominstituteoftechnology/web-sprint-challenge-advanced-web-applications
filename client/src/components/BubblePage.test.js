fca5dd0a5cbddcad554f157731f5d5ab28f6c7bf
Simone Ballard <simone.a.ballard@gmail.com>
2020-10-16 11:19:20 -0600

    test built in BubblePage.test.js

--- client/src/components/BubblePage.test.js
+++ client/src/components/BubblePage.test.js
@@ -2,6 +2,32 @@ import React from "react";
 import { render, screen } from "@testing-library/react";
 import BubblePage from "./BubblePage";
 
+import { fetchColors as mockFetchColors } from {BubblePage};
+
 test("Fetches data and renders the bubbles", () => {
   // Finish this test
+
+jest.mock('./fetchColors');
+    describe("Test App", ()=>{
+        test("renders without errors", ()=>{
+            render(<App />);
 });
+
+test('fetches colors', async () => {
+    render(<App />);
+
+    const newColors = {data: [
+      {
+      color: "aliceblue",
+      code: {
+        hex: "#f0f8ff"
+      },
+      id: 1
+    }
+    ]
+
+};
+  });
+  
+  mockFetchColors.mockResolvedValueOnce(newColors);
+    };
--- client/src/components/ColorList.js
+++ client/src/components/ColorList.js
@@ -11,6 +11,7 @@ const ColorList = ({ colors, updateColors }) => {
   console.log(colors);
   const [editing, setEditing] = useState(false);
   const [colorToEdit, setColorToEdit] = useState(initialColor);
+  
   const { id } = useParams();
   const { push } = useHistory();
 
