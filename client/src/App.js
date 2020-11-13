56bddc5d15b875a07339d914bf95aacc16b2a172
mbenson3434 <44735865+mbenson3434@users.noreply.github.com>
2020-11-13 11:51:55 -0500

    Stage 1 done

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,18 @@
 import React, { useState } from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
-
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Login from "./components/Login";
 import "./styles.scss";
+import PrivateRoute from './components/PrivateRoute';
+import BubblePage from './components/BubblePage';
 
 function App() {
   return (
     <Router>
       <div className="App">
-        <Route exact path="/" component={Login} />
-        {/* 
-          Build a PrivateRoute component that will 
-          display BubblePage when you're authenticated 
-        */}
+        <Switch>
+          <Route exact path="/" component={Login} />
+          <PrivateRoute path="/bubblepage" component={BubblePage}/>
+        </Switch>
       </div>
     </Router>
   );
--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,12 +1,61 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from 'axios';
+import { useHistory } from 'react-router-dom';
+
+// username: 'Lambda School', password: 'i<3Lambd4'
+const initialValues = {
+  credentials: {username: '', password: ''}
+}
 
 const Login = () => {
   // make a post request to retrieve a token from the api
   // when you have handled the token, navigate to the BubblePage route
+const [formValues, setFormValues] = useState(initialValues);
+const { push } = useHistory();
+
+const submitForm = (e) => {
+  e.preventDefault();
+  useEffect(()=> {
+    axios
+    .get('http://localhost:5000/api/login', formValues.credentials)
+      .then(res => {
+        localStorage.setItem("token", res.data.payload);
+        push("/colors");
+      })
+      .catch(err => console.log(err));
+    
+  }, []);
+};
+
+  const handleChange = (e) => {
+    setFormValues({
+      credentials: {
+        ...formValues.credentials,
+        [e.target.name]: e.target.value
+      }
+    });
+  };
+
   return (
     <>
       <h1>Welcome to the Bubble App!</h1>
-      <p>Build a login page here</p>
+      <form onSubmit={submitForm}>
+        <input
+        type="text"
+        name="username"
+        onChange={handleChange}
+        value={formValues.username}
+        placeholder="username"
+        />
+        <input
+        type="password"
+        name="password"
+        onChange={handleChange}
+        value={formValues.password}
+        placeholder="password"
+        />
+        <button onClick={()=>push("/bubblepage")}>Log In</button>
+      </form>
     </>
   );
 };
