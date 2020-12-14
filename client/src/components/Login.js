a2769286cf15d1c33e30e4232b1d6142029ed9df
Austin Littlefield <austinlittlefield@MacBook-Pro.local>
2020-12-13 16:10:33 -0800

    login form created

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,14 +1,53 @@
-import React from "react";
+import React, {useState} from "react";
 
 const Login = () => {
-  // make a post request to retrieve a token from the api
-  // when you have handled the token, navigate to the BubblePage route
+
+const [form, setForm] = useState({
+  username: "",
+  password: ""
+})
+
+
+let onChange = (e) => {
+  e.preventDefault()
+  setForm({
+    ...form,
+    [e.target.name]: e.target.value
+  })
+}
+
+
+
+
   return (
-    <>
-      <h1>Welcome to the Bubble App!</h1>
-      <p>Build a login page here</p>
-    </>
+    <div>
+      <form>
+        <input
+        name="username"
+        type="text"
+        placeholder="Username"
+        value={form.username}
+        onChange= {onChange}
+        />
+        <input
+        name="username"
+        type="text"
+        placeholder="Password"
+        value={form.Password}
+        onChange= {onChange}
+        />
+        <button>Login!</button>
+      </form>
+    </div>
   );
 };
 
 export default Login;
+
+
+  // make a post request to retrieve a token from the api
+  // when you have handled the token, navigate to the BubblePage route
+  // need axios request
+  // need handle change function
+  // need login function
+  // need to write out log in
