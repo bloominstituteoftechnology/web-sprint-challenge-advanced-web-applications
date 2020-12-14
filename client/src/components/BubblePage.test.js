6d61a54c12bd5e29471907eb8341799d002e15df
Austin Littlefield <austinlittlefield@MacBook-Pro.local>
2020-12-13 18:19:49 -0800

    tests

--- client/src/components/BubblePage.test.js
+++ client/src/components/BubblePage.test.js
@@ -3,5 +3,5 @@ import { render, screen } from "@testing-library/react";
 import BubblePage from "./BubblePage";
 
 test("Fetches data and renders the bubbles", () => {
-  // Finish this test
+  render(<BubblePage/>)
 });
