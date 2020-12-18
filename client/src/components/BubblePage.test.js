d3945735b0b00c2988972807700ae217b78b127d
Sonyei <esonyei@gmail.com>
2020-12-18 13:41:13 -0500

    tests are evil and we hateses them

--- client/src/components/BubblePage.test.js
+++ client/src/components/BubblePage.test.js
@@ -1,7 +1,71 @@
 import React from "react";
-import { render, screen } from "@testing-library/react";
+import { render, screen, waitFor } from "@testing-library/react";
 import BubblePage from "./BubblePage";
+import userEvent from '@testing-library/user-event'
+import { axiosWithAuth } from '../utils/axiosWithAuth'
+
+//GET the color first.
+const getColor = () => {
+  axiosWithAuth()
+    .get('/colors')
+    .then(res => console.log(res.data))
+    .catch(err => console.log('ERROR: ', err))
+}
+
+const mockGetColors = getColor()
+jest.mock(mockGetColors)
+
+test('should render on the page', () => {
+  render(<BubblePage colors={[]}/>)
+})
 
 test("Fetches data and renders the bubbles", () => {
   // Finish this test
+
+  const mockColor = [
+  {
+    color: "bisque",
+    code: {
+      hex: "#dd9a99"
+    },
+    id: 7
+  },
+  {
+    color: "softyellow",
+    code: {
+      hex: "#dcdd99"
+    },
+    id: 8
+  },
+  {
+    color: "blanchedalmond",
+    code: {
+      hex: "#ffebcd"
+    },
+    id: 9
+  },
+  {
+    color: "blue",
+    code: {
+      hex: "#6093ca"
+    },
+    id: 10
+  },
+  {
+    color: "blueviolet",
+    code: {
+      hex: "#8a2be2"
+    },
+    id: 11
+  }
+]
+
+const { rerender } = render(<BubblePage mockColor={[]} />)
+
+rerender(<BubblePage mockColor={mockColor} />)
+
+const bubbleRender = screen.getByText(/bubbles/i)
+expect(bubbleRender).toBeInTheDocument()
+
+
 });
