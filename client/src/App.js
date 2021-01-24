1c6187a0b17e2213c0d9c663705846f06ef700d0
Amanda Nelson <amanda.gaylord@gmail.com>
2021-01-24 18:58:03 -0500

    exact paths updated on App component

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,18 @@
 import React, { useState } from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 import Login from "./components/Login";
 import "./styles.scss";
+import BubblePage from "./components/BubblePage";
+import PrivateRoute from "./utils/PrivateRoute";
 
 function App() {
   return (
     <Router>
-      <div className="App">
-        <Route exact path="/" component={Login} />
-        {/* 
-          Build a PrivateRoute component that will 
-          display BubblePage when you're authenticated 
-        */}
-      </div>
+      <Switch>
+        <Route exact path="/" components={Login} />
+        <PrivateRoute exact path="/bubble-page" components={BubblePage} />
+      </Switch>
     </Router>
   );
 }
