e1c64199c1d677b97df581ffab9e964f551823ef
Amanda Nelson <amanda.gaylord@gmail.com>
2021-01-24 16:55:17 -0500

    client/index.js updated with React.StrictMode and Router

--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,15 @@
 import React, { useState } from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
+import { BrowserRouter as Router } from "react-router-dom";
 
 const rootElement = document.getElementById("root");
-ReactDOM.render(<App />, rootElement);
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Router>
+      <App />
+    </Router>
+  </React.StrictMode>,
+  rootElement
+);
