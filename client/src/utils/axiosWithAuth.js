60db9801d58b3081794728272538dc2a084c2f42
Amanda Nelson <amanda.gaylord@gmail.com>
2021-01-24 22:16:46 -0500

    Found my bug in axiosWithAuth - I didn't set the base url to the /api endpoint

--- client/src/utils/axiosWithAuth.js
+++ client/src/utils/axiosWithAuth.js
@@ -1,12 +1,12 @@
 import axios from "axios";
 export const axiosWithAuth = () => {
-const token = window.localStorage.getItem("token");
+  const token = window.localStorage.getItem("token");
 
   return axios.create({
     headers: {
       authorization: token,
     },
-    baseURL: "http://localhost:5000",
+    baseURL: "http://localhost:5000/api",
   });
 };
 
