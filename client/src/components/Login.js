1d0545b8c766f389fc1fa9db17ca26cfff16c4f0
Amanda Nelson <amanda.gaylord@gmail.com>
2021-01-24 21:26:59 -0500

    changed Login component

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -2,54 +2,61 @@ import React, { useState } from "react";
 import { axiosWithAuth } from "../utils/axiosWithAuth";
 import { useHistory } from "react-router-dom";
 
-const initialState = {
-  username: "",
-  password: "",
-};
-
-const Login = () => {
+class Login extends React.Component {
   // make a post request to retrieve a token from the api
   // when you have handled the token, navigate to the BubblePage route
-  const [login, setLogin] = useState(initialState);
-  const history = useHistory();
-  const handleChanges = (e) => {
-    setLogin({ ...login, [e.target.name]: e.target.value });
+  state = {
+    login: {
+      username: "",
+      password: "",
+    },
   };
-  const handleSubmit = (e) => {
+
+  handleChanges = (e) => {
+    this.setState({
+      login: {
+        ...this.state.login,
+        [e.target.name]: e.target.value,
+      },
+    });
+  };
+
+  handleSubmit = (e) => {
     e.preventDefault();
     axiosWithAuth()
-      .post("/api/login", login)
+      .post("/api/login", this.state.login)
       .then((res) => {
-        localStorage.setItem("token", res.data.payload);
-        history.push("/bubble-page");
+        window.localStorage.setItem("token", res.data.payload);
+        this.props.history.push("/bubbles");
       })
       .catch((err) => {
         console.log(err.response);
       });
   };
-
-  return (
-    <>
-      <h1>Welcome to the Bubble App!</h1>
-      <form onSubmit={handleSubmit}>
-        <input
-          name="username"
-          type="text"
-          value={login.username}
-          onChange={handleChanges}
-          placeholder="username"
-        />
-        <input
-          name="password"
-          type="password"
-          value={login.password}
-          onChange={handleChanges}
-          placeholder="password"
-        />
-        <button type="submit">Login</button>
-      </form>
-    </>
-  );
-};
+  render() {
+    return (
+      <>
+        <h1>Welcome to the Bubble App!</h1>
+        <form onSubmit={handleSubmit}>
+          <input
+            name="username"
+            type="text"
+            value={login.username}
+            onChange={handleChanges}
+            placeholder="username"
+          />
+          <input
+            name="password"
+            type="password"
+            value={login.password}
+            onChange={handleChanges}
+            placeholder="password"
+          />
+          <button type="submit">Login</button>
+        </form>
+      </>
+    );
+  }
+}
 
 export default Login;
