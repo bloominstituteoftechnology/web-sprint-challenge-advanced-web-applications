3400943d8954f36257be0771f35330e42641c79e
Amanda Nelson <amanda.gaylord@gmail.com>
2021-01-24 19:26:03 -0500

    ColorList component updated with axiosWithAuth

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,11 @@
 import React, { useState } from "react";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Redirect,
+  Route,
+  Switch,
+  Link,
+} from "react-router-dom";
 
 import Login from "./components/Login";
 import "./styles.scss";
@@ -9,9 +15,19 @@ import PrivateRoute from "./utils/PrivateRoute";
 function App() {
   return (
     <Router>
+      <nav>
+        <p>
+          <Link to="login">Login</Link>
+        </p>
+        <p>
+          <Link to="/bubbles">Bubbles</Link>
+        </p>
+      </nav>
       <Switch>
+        <PrivateRoute exact path="/bubbles" components={BubblePage} />
+        <Redirect exact from="/bubbles/reload" to="/bubbles" />
         <Route exact path="/" components={Login} />
-        <PrivateRoute exact path="/bubble-page" components={BubblePage} />
+        <Route component={Login} />
       </Switch>
     </Router>
   );
