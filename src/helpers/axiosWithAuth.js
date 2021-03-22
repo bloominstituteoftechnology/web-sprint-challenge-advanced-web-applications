81a62b6b28cc8eb3dc368efd788a2f2b860ff29b
Kelsey Nielsen <kelseywnielsen@gmail.com>
2021-03-21 18:37:03 -0600

    added axios with auth

--- src/helpers/axiosWithAuth.js
+++ src/helpers/axiosWithAuth.js
@@ -1,4 +1,12 @@
 import axios from "axios";
 
+export const axiosWithAuth = () => {
+    const token = localStorage.getItem("authToken");
+    return axios.create({
+        baseURL: "http://localhost:5000/api",
+        headers: { Authorization: token }
+    });
+}
+
 //Task List:
 //Build and export a function used to send in our authorization token
