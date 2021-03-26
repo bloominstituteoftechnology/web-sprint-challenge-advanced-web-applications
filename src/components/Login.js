807072a1979a6cf18e2250df00fbd83ac56be31c
Tarah <tarah.agbokhana@gmail.com>
2021-03-26 18:46:07 -0400

    Updated Login

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import axios from 'axios';
+import { axiosWithAuth } from '../helpers/axiosWithAuth';
 import { useHistory } from 'react-router-dom';
 
 const Login = () => {
