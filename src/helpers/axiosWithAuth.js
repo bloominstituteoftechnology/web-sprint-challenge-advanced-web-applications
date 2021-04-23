346cb2b7bc6ac5fc3e944e79aa19695f1bc9c2b8
Wynstona Louis <wynstona@Wynstonas-MacBook-Pro.local>
2021-04-23 11:47:57 -0400

    Render BubblePage as a PrivateRoute

--- src/helpers/axiosWithAuth.js
+++ src/helpers/axiosWithAuth.js
@@ -2,3 +2,12 @@ import axios from "axios";
 
 //Task List:
 //Build and export a function used to send in our authorization token
+export const axiosWithAuth = () => {
+	const token = localStorage.getItem("token");
+
+	return axios.create({
+		headers: {
+			Authorization: token,
+		},
+	});
+};
