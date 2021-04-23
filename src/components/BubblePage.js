a9691d30dd0f630e9dde1ad9733a767d404bc91a
coryp4 <cory@burtano.com>
2021-04-23 14:05:19 -0700

    first

--- src/components/BubblePage.js
+++ src/components/BubblePage.js
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 
 import Bubbles from "./Bubbles";
 import ColorList from "./ColorList";
+const secondBubbles = {}
 
 const BubblePage = () => {
   const [colorList, setColorList] = useState([]);
@@ -15,6 +16,6 @@ const BubblePage = () => {
 };
 
 export default BubblePage;
-
+//Cory's initial notes for changes
 //Task List:
 //1. When the component mounts, make an axios call to retrieve all color data and push to state.
