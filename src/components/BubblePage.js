62aa94386628e64dd5bfaaf252ddcf879d05c47d
Wynstona Louis <wynstona@Wynstonas-MacBook-Pro.local>
2021-04-23 12:03:40 -0400

    RRetrieved color list with hook fetchColors and setColorList to the res.data

--- src/components/BubblePage.js
+++ src/components/BubblePage.js
@@ -1,21 +1,51 @@
 import React, { useEffect, useState } from "react";
-import axios from "axios";
 
 import Bubbles from "./Bubbles";
 import ColorList from "./ColorList";
+import { fetchColors } from "../helpers/fetchColors";
 
 const BubblePage = () => {
-  const [colorList, setColorList] = useState([]);
+	const [colorList, setColorList] = useState([]);
 
-  return (
-    <>
-      <ColorList colors={colorList} updateColors={setColorList} />
-      <Bubbles colors={colorList} />
-    </>
-  );
+	useEffect(() => {
+		fetchColors()
+			.then((res) => {
+				console.log(res.data);
+				setColorList(res.data);
+			})
+			.catch((err) => {
+				console.log("Error fetching", err);
+			});
+	}, []);
+
+	return (
+		<>
+			<ColorList colors={colorList} updateColors={setColorList} />
+			<Bubbles colors={colorList} />
+		</>
+	);
 };
 
 export default BubblePage;
+
+// import React, { useEffect, useState } from "react";
+// import axios from "axios";
+
+// import Bubbles from "./Bubbles";
+// import ColorList from "./ColorList";
+
+// const BubblePage = () => {
+//   const [colorList, setColorList] = useState([]);
+
+//   return (
+//     <>
+//       <ColorList colors={colorList} updateColors={setColorList} />
+//       <Bubbles colors={colorList} />
+//     </>
+//   );
+// };
+
+// export default BubblePage;
 
 //Task List:
 //1. Make an axios call to retrieve all color data and push to state on mounting.
