0100e09ebcdde7349b2f18f622d0124575c3f1aa
Wynstona Louis <wynstona@Wynstonas-MacBook-Pro.local>
2021-04-23 11:51:26 -0400

    Built an export function used to send in our authorization token

--- src/helpers/axiosWithAuth.js
+++ src/helpers/axiosWithAuth.js
@@ -1,7 +1,5 @@
 import axios from "axios";
 
-//Task List:
-//Build and export a function used to send in our authorization token
 export const axiosWithAuth = () => {
 	const token = localStorage.getItem("token");
 
