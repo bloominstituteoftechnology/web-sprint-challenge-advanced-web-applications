e51eff29bf66a75e07db887699ef1725f7d9a58d
TIMOTHY T JACKREECE <73547670+tjackreece@users.noreply.github.com>
2021-04-23 12:06:04 -0400

    Make an axios call to retrieve all color data and push to state on mounting.

--- src/components/BubblePage.js
+++ src/components/BubblePage.js
@@ -28,24 +28,3 @@ const BubblePage = () => {
 
 export default BubblePage;
 
-// import React, { useEffect, useState } from "react";
-// import axios from "axios";
-
-// import Bubbles from "./Bubbles";
-// import ColorList from "./ColorList";
-
-// const BubblePage = () => {
-//   const [colorList, setColorList] = useState([]);
-
-//   return (
-//     <>
-//       <ColorList colors={colorList} updateColors={setColorList} />
-//       <Bubbles colors={colorList} />
-//     </>
-//   );
-// };
-
-// export default BubblePage;
-
-//Task List:
-//1. Make an axios call to retrieve all color data and push to state on mounting.
