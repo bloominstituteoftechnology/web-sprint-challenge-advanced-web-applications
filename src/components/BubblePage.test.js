6d89dde34248e74d5d851d44ce0af1e8bf5cd044
ErikBurdett <erikaburdet@gmail.com>
2021-04-23 14:34:12 -0500

    Create a CodeGrade submission

--- src/components/BubblePage.test.js
+++ src/components/BubblePage.test.js
@@ -12,3 +12,5 @@ test("Fetches data and renders the bubbles on mounting", () => {
     const colors = screen.getByText(/colors/i)
     expect(colors).toBeInTheDocument()
 }); 
+
+/// for codegrade push
