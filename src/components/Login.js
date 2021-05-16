939da14f7985c40b5d9fc2bfbb7948c06470977c
Sarah Mucho <64818980+SJMucho@users.noreply.github.com>
2021-05-16 15:13:16 -0400

    auth function

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,4 +1,5 @@
 import React, { useEffect } from "react";
+import axios from "axios";
 
 const Login = () => {
   // make a post request to retrieve a token from the api
--- src/helpers/axiosWithAuth.js
+++ src/helpers/axiosWithAuth.js
@@ -2,3 +2,15 @@ import axios from "axios";
 
 //Task List:
 //Build and export a function used to send in our authorization token
+
+export const axiosWithAuth = () => {
+  const token = localStorage.getItem("token");
+
+  return axios.create({
+    baseURL: "http://localhost:5000/api",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `${token}`,
+    },
+  });
+};
