bacd9394b2f6a1c821b40973964698cc7212c8e4
Sarah Mucho <64818980+SJMucho@users.noreply.github.com>
2021-05-16 15:25:15 -0400

    PrivateRoute

--- src/components/PrivateRoute.js
+++ src/components/PrivateRoute.js
@@ -1,11 +1,22 @@
+import React from "react";
+import { Route, Redirect } from "react-router-dom";
 
+export const PrivateRoute = (props) => {
+  const { component: Component, ...rest } = props;
 
-
-
-
-
-
-
+  return (
+    <Route
+      {...rest}
+      render={(renderProps) => {
+        if (localStorage.getItem("token")) {
+          return <Component {...renderProps} />;
+        } else {
+          return <Redirect to="/" />;
+        }
+      }}
+    />
+  );
+};
 
 //Task List:
 //1. Build a PrivateRoute component that redirects if user is not logged in
