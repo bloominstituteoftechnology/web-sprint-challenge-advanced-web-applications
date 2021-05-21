7719d0e153174d096db45e1c76f15178241421ac
William Dye <williamdye@williams-mbp.myfiosgateway.com>
2021-05-21 15:11:50 -0400

    Final push

--- src/components/BubblePage.test.js
+++ src/components/BubblePage.test.js
@@ -1,13 +1,24 @@
 import React from "react";
-import { render, screen } from "@testing-library/react";
+import { render, screen, waitFor } from "@testing-library/react";
 import BubblePage from "./BubblePage";
 
 test("Renders BubblePage without errors", () => {
   render(<BubblePage />);
 });
 
-test("Fetches data and renders the bubbles on mounting", () => {
+test("Fetches data and renders the bubbles on mounting", async () => {
   render(<BubblePage />);
+
+  await waitFor(() => {
+    const colorTitle = screen.queryByText(/colors/i);
+
+    const firstColor = screen.queryByText(/softpink/i);
+
+    expect(colorTitle).toBeInTheDocument();
+
+    expect(firstColor).toBeInTheDocument();
+
+  })
 });
 
 //Task List
