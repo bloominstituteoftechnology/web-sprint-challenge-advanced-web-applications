2db4a8b12dc35c27efab2d7750196a523e9f37a1
ricardo-pena <ricardo.penagarcia06@gmail.com>
2021-05-21 13:37:06 -0400

    MVP

--- src/App.js
+++ src/App.js
@@ -1,19 +1,29 @@
 import React, { useState } from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route,Switch, useHistory } from "react-router-dom";
+import BubblePage from './components/BubblePage';
+
+import PrivateRoute from './components/PrivateRoute'
 
 import Login from "./components/Login";
 import "./styles.scss";
 
 function App() {
+
+  const logoutFunc = localStorage.removeItem('token');
+
   return (
     <Router>
       <div className="App">
         <header>
           Color Picker Sprint Challenge
-          <a data-testid="logoutButton" href="#">logout</a>
+          <a onClick = {logoutFunc} data-testid="logoutButton" href="/">logout</a>
         </header> 
-
+        <Switch>
+        
         <Route exact path="/" component={Login} />
+        <Route exact path="/login" component={Login} />
+        <PrivateRoute exact path='/protected' component={BubblePage}/>
+        </Switch>
       </div>
     </Router>
   );
--- src/components/BubblePage.test.js
+++ src/components/BubblePage.test.js
@@ -1,13 +1,44 @@
 import React from "react";
-import { render, screen } from "@testing-library/react";
+import { render, screen, waitFor } from "@testing-library/react";
 import BubblePage from "./BubblePage";
+
+import {fetchColors as fakeFetchColors} from './fetchColors'
+jest.mock('./fetchColors')
+
+const fakeColors = [
+  {
+    color: "aliceblue",
+    code: {
+      hex: "#f0f8ff",
+    },
+    id: 1,
+  },
+  {
+    color: "limegreen",
+    code: {
+      hex: "#99ddbc",
+    },
+    id: 2,
+  },
+  {
+    color: "aqua",
+    code: {
+      hex: "#00ffff",
+    },
+    id: 3,
+  }
+]
 
 test("Renders BubblePage without errors", () => {
   // Finish this test
+  render(<BubblePage/>);
 });
 
-test("Fetches data and renders the bubbles on mounting", () => {
+test("Fetches data and renders the bubbles on mounting", async() => {
   // Finish this test
+  render(<BubblePage/>)
+  fakeFetchColors.mockResolvedValueOnce(fakeColors)
+
 });
 
 //Task List
