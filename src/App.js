ad85bb47ad37d5c18a3a3a74968f1ed5773a7805
John Vehmeyer <j.vehmeyer@gmail.com>
2021-06-18 05:31:37 -0500

    built private route and imported to app.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, { useState } from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 
+import PrivateRoute from "./components/PrivateRoute";
+
 import Login from "./components/Login";
 import "./styles.scss";
 
