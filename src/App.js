e7bc66cd256bd26761c84cdbf02958f1c8c17699
SavvyPlusPlus <info@savvydc.com>
2021-07-18 16:08:55 -0400

    MVP met

--- src/App.js
+++ src/App.js
@@ -1,18 +1,24 @@
 import React, { useState } from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
-
+import BubblePage from "./components/BubblePage";
 import Login from "./components/Login";
+import PrivateRoute from "./components/PrivateRoute";
 import "./styles.scss";
 
 function App() {
+
+  const logout = () => {
+    localStorage.removeItem("token");
+  }
+
   return (
     <Router>
       <div className="App">
         <header>
           Color Picker Sprint Challenge
-          <a data-testid="logoutButton" href="#">logout</a>
-        </header> 
-
+          <a onClick={logout} data-testid="logoutButton" href="http://localhost:3000/">logout</a>
+        </header>
+        <PrivateRoute exact path="/bubblepage" component={BubblePage}/>
         <Route exact path="/" component={Login} />
       </div>
     </Router>
--- src/components/ColorList.test.js
+++ src/components/ColorList.test.js
@@ -1,14 +1,30 @@
 import React from 'react';
 import MutationObserver from 'mutationobserver-shim';
-
-import { render, screen} from "@testing-library/react";
+import EditMenu from './EditMenu';
+import userEvent from '@testing-library/user-event';
+import { render, screen } from "@testing-library/react";
 import ColorList from './ColorList';
 
+const testColor = {
+    color: "blue",
+    code: { hex: "#7fffd4" },
+    id: 1
+}
+
+const noColor = [];
+
 test("Renders an empty list of colors without errors", () => {
+    render(<ColorList color={noColor} />)
 });
 
 test("Renders a list of colors without errors", () => {
+    render(<ColorList color={testColor} />)
 });
 
 test("Renders the EditForm when editing = true and does not render EditForm when editing = false", () => {
+    const toggleEdit = jest.fn()
+    render(<ColorList color={testColor} />);
+    let editing = screen.queryByTestId("color");
+    userEvent.click(editing);
+    expect(toggleEdit).toBeCalled();
 });
