3181a6192932281722e57ab82438bdbc0cb614ad
Angela Engel <amengel.ae@icloud.com>
2021-08-15 11:11:57 -0600

    error fixed

--- src/helpers/axiosWithAuth.js
+++ src/helpers/axiosWithAuth.js
@@ -2,3 +2,15 @@ import axios from "axios";
 
 //Task List:
 //Build and export a function used to send in our authorization token
+const axiosWithAuth = () => {
+    const token = localStorage.getItem('token');
+
+    return axios.create({
+        headers: {
+            Authorization: token
+        },
+        baseURL: 'http://localhost:3000'
+    })
+}
+
+export default axiosWithAuth;
