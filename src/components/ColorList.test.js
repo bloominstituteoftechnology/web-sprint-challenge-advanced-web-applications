30ebf0757a686ac44f5f015248cae622072ca720
Angela Engel <amengel.ae@icloud.com>
2021-08-15 11:46:42 -0600

    done

--- src/components/ColorList.test.js
+++ src/components/ColorList.test.js
@@ -4,11 +4,24 @@ import MutationObserver from 'mutationobserver-shim';
 import { render, screen} from "@testing-library/react";
 import ColorList from './ColorList';
 
+const testColor = {
+    color: "blue",
+    code: { hex: "#7fffd4" },
+    id: 1
+}
+
 test("Renders an empty list of colors without errors", () => {
+    render(<ColorList color={noColor} />)
 });
 
 test("Renders a list of colors without errors", () => {
+    render(<ColorList color={testColor} />)
 });
 
 test("Renders the EditForm when editing = true and does not render EditForm when editing = false", () => {
+    const toggleEdit = jest.fn()
+    render(<ColorList color={testColor} />);
+    let editing = screen.queryByTestId("color");
+    userEvent.click(editing);
+    expect(toggleEdit).toBeCalled();
 });
--- src/helpers/axiosWithAuth.js
+++ src/helpers/axiosWithAuth.js
@@ -9,7 +9,7 @@ const axiosWithAuth = () => {
         headers: {
             Authorization: token
         },
-        baseURL: 'http://localhost:3000'
+        baseURL: 'http://localhost:5000'
     })
 }
 
