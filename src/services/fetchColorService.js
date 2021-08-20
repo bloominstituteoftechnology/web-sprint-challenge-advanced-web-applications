8d9432e535ca748550377a910cf4d65492166742
Rick <rmansfield@realpeoplesratings.com>
2021-08-20 10:09:58 -0500

    Built out fetchColorService.js

--- src/services/fetchColorService.js
+++ src/services/fetchColorService.js
@@ -1,6 +1,16 @@
 import axiosWithAuth from '../helpers/axiosWithAuth';
 
-const fetchColorService = () => {
+const fetchColorService = (getColors) => {
+    axiosWithAuth()
+    .get(`/api/colors`)
+    .then((res)=>{
+        console.log('fetchColorsServices res:', res);
+        getColors(res.data)
+        console.log('this is the setColors res.data', res.data);
+    })
+    .catch(err => {
+        console.log(err);
+    })
     
 }
 
