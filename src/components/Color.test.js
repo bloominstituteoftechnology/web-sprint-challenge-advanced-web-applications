db30fe53b72c4fb25915eb6a3da2a119893c1e4f
Rick <rmansfield@realpeoplesratings.com>
2021-08-20 10:50:04 -0500

    Color.test.js test #3 complete/passing

--- src/components/Color.test.js
+++ src/components/Color.test.js
@@ -35,6 +35,30 @@ test("Renders the color passed into component", () => {
 
 });
 test("Executes handleDelete and toggleEdit property when the 'x' icon is clicked", () => {
+    //Arrange - Usually Render/const/screen to prep for Action step (review Day 1 (Monday) testing Display.test.js)
+    const mockToggleFunc = jest.fn();
+    const mockDeleteFunc = jest.fn();
+    render(
+        <Color
+            color={{
+                code: { hex: "" },
+                color: "",
+                id: 1,
+            }}
+            deleteColor={mockDeleteFunc}
+            toggleEdit={mockToggleFunc}
+        />
+    );
+    let theXToDelete = screen.getByText(/x/i);//it says "icon" on line 39 but it's a text letter "x". 
+
+    //Act - Usually doing something to what you just const/screened/arranged
+    userEvent.click(theXToDelete);
+
+    //Assert - What's "expected"
+    expect(mockToggleFunc).toHaveBeenCalled();
+    expect(mockDeleteFunc).toHaveBeenCalled();
+    expect(mockDeleteFunc.mock.calls).toHaveLength(1);
+    expect(mockToggleFunc.mock.calls).toHaveLength(1);
 });
 
 test("Executes setEditColor and toggleEdit property when color div is clicked", () => {
