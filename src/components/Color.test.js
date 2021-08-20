3e613fdfc8c943effb20b39170f9f9866df74103
Rick <rmansfield@realpeoplesratings.com>
2021-08-20 10:43:56 -0500

    Color.test.js #1 complete/passing

--- src/components/Color.test.js
+++ src/components/Color.test.js
@@ -5,9 +5,18 @@ import { render, screen, waitFor } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 import Color from './Color';
 
+//I inpspected the res.data to see the needed syntax/structure.See my pic of this data in readme.md preview mode under testing. I created a TOC Table of Contents for easy reference. 
 test("Renders without errors with blank color passed into component", () => {
+    render(
+        <Color
+            color={{
+                code: { hex: "" },
+                color: "",
+                id: 0,
+            }}
+        />
+    );
 });
-  
 test("Renders the color passed into component", () => {
 });
 
