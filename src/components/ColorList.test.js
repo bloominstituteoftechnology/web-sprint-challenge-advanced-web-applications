2d17fa3c17c4d245fd8d542a467cd7f1f746824c
Rick <rmansfield@realpeoplesratings.com>
2021-08-20 12:21:27 -0500

    ColorList.test.js #1 & #2 complete/passing

--- src/components/ColorList.test.js
+++ src/components/ColorList.test.js
@@ -3,11 +3,24 @@ import MutationObserver from 'mutationobserver-shim';
 
 import { render, screen} from "@testing-library/react";
 import ColorList from './ColorList';
+const testColors = [
+    {code: {hex: "#blanchedalmond"},
+    color: "aliceblue",
+    id: 8},
+    {code: {hex: "#6093ca"},
+    color: "blue",
+    id: 9},
+    {code: {hex: "#ff000"},
+    color: "red",
+    id: 10}
+];
 
 test("Renders an empty list of colors without errors", () => {
+    render(<ColorList colors={[]} />)
 });
 
 test("Renders a list of colors without errors", () => {
+    render(<ColorList colors={testColors} />)
 });
 
 test("Renders the EditForm when editing = true and does not render EditForm when editing = false", () => {
