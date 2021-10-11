eee857c0dcbb2610e68b24b769204984849b9801
vida <ofo28@aol.com>
2021-10-11 02:21:48 -0400

    update articleservices

--- src/services/articleServices.js
+++ src/services/articleServices.js
@@ -1,6 +1,12 @@
 
 
 const articleService = ()=> {
+    axiosWithAuth.get(`http://localhost:5000/api/articles/${id}`)
+    .then(res => {
+      props.setArticles(res.data)
+      push('/articles')
+    })
+    .catch(err => console.log(err))
 }
 
 export default articleService;
